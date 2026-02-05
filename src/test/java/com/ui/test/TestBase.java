a999659c64656adc09425ff60b9026732f4f87e0
MD <muhiuddinanik98@gmail.com>
2026-02-05 21:51:44 +0600

    feat(testbase): implement dynamic parameter injection for setup
    - Updated setup() method to accept TestNG parameters: browser, isLambdaTest, isHeadLess, and ENV.
    - Added @Optional annotations to ensure tests run successfully in IDEs without explicit parameters.
    - Passed 'ENV' variable to HomePage constructor to enable environment-specific configuration (QA/DEV).

--- src/test/java/com/ui/test/TestBase.java
+++ src/test/java/com/ui/test/TestBase.java
@@ -1,8 +1,5 @@
 package com.ui.test;
 
-import static com.constants.Browser.CHROME;
-import static com.constants.Browser.FIREFOX;
-
 import org.apache.logging.log4j.Logger;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -23,22 +20,23 @@ public class TestBase {
 	// 🎛️ CONTROL PANEL
 	// Set these to TRUE or FALSE to control your entire framework
 
-	@Parameters({ "browser", "isLambdaTest","isHeadLess" })
+	@Parameters({ "browser", "isLambdaTest", "isHeadLess", "ENV" })
 	@BeforeMethod(description = "Load the Home page of the website")
 
-	public void setup(
-			@Optional("chrome") String browser, 
+	public void setup(@Optional("chrome") String browser, 
 			@Optional("false") boolean isLambdaTest,
-			@Optional("true")  boolean isHeadLess, ITestResult result) {
+			@Optional("true") boolean isHeadLess, 
+			@Optional("DEV") String ENV, 
+			ITestResult result) {
 
 		String testName = result.getMethod().getMethodName();
 
-		logger.info(
-				"⚙️ SETUP: Initializing Test Environment [Cloud: " + isLambdaTest + " | Headless: " + isHeadLess + "]");
+		logger.info("⚙️ SETUP: Initializing Test Environment [Cloud: " + isLambdaTest + " | Headless: " + isHeadLess
+				+ " | Environment: " + ENV + "]");
 
-		// ✅ ONE LINE TO RULE THEM ALL
+		//ONE LINE TO RULE THEM ALL
 		// The HomePage/BrowserUtility now handles the 'if/else' logic internally
-		homePage = new HomePage(Browser.valueOf(browser.toUpperCase()), isHeadLess, isLambdaTest, testName);
+		homePage = new HomePage(Browser.valueOf(browser.toUpperCase()), isHeadLess, isLambdaTest, testName,ENV);
 
 		logger.info("🛠 SETUP: Session Created Successfully ✅.");
 	}
